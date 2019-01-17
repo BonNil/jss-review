488c4fe66887f2e4f1553484b5aeb78aa5eb3c51
Nilsson Bonny <bonny.nilsson@stenaline.com>
2019-01-17 11:32:25 +0100

    fixed metadata file name

--- sitecore/pipelines/generateManifest/generateLayouts.patch.js
+++ sitecore/pipelines/generateManifest/generateLayouts.patch.js
@@ -1,18 +1,15 @@
-import Layouts from '../../../src/layouts/metaData.js';
+import Layouts from '../../../src/layouts/metadata.js';
 
 // This function is invoked by JSS convention:
 // ./sitecore/pipelines/**/*.patch.js are automatically invoked when pipelines are initializing.
 export const config = (pipelines) => {
   const pipeline = pipelines.getPipeline('generateManifest');
-  console.log(pipelines);
-
   pipeline.addProcessor({
     name: 'generateLayouts',
     process: function (args) {
       console.log('inline process patch: generateLayouts');
-      console.log(Layouts);
+      
       const finalLayouts = [];
-
       for (var key in Layouts){
         finalLayouts.push(Layouts[key]);
       }
